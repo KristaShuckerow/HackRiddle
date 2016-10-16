259dd4a4e913205f2b2292b74f4b24895821c57f
Krista Shuckerow <kshuckerow@gmail.com>
2016-10-16 01:46:48 -0400

    Update Definition.R

--- Definition.R
+++ Definition.R
@@ -1,10 +1,12 @@
 #CALLING API for dictionary
 #reading in word via command line
 #install.packages('jsonlite', repos='http://cran.us.r-project.org')
-#library(jsonlite)
-newWord = commandArgs()
+library(jsonlite)
+#newWord = commandArgs()
+args <- commandArgs(TRUE)
+newWord <-args[1]
 cat(newWord)
-q()
+#q()
 wordSearch <- "http://api.wordnik.com:80/v4/word.json/query/definitions?limit=200&includeRelated=true&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5"
 newSearch <- gsub("query", newWord, wordSearch)
 wordDef <- fromJSON(newSearch)
